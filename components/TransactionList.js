57bee60bd8a6c4ab917cadcc77786e2a14356b6c
Kadar1983 <kadar.zh@gmail.com>
2025-12-05 14:31:32 +0100

    Update TransactionList.js

--- components/TransactionList.js
+++ components/TransactionList.js
@@ -1,25 +1,3 @@
-import React, { useEffect, useState } from "react";
-import { getUserTransactions } from "../api/transactions";
-
 export default function TransactionList({ userId }) {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function load() {
-      const t = await getUserTransactions(userId);
-      setData(t);
-    }
-    load();
-  }, [userId]);
-
-  return (
-    <div className="mt-4">
-      <h2 className="font-semibold mb-2">المعاملات</h2>
-      {data.map(t => (
-        <p key={t.id}>
-          {t.date} - {t.type} - {t.amount} Pi
-        </p>
-      ))}
-    </div>
-  );
+  return <div>لا توجد معاملات.</div>;
 }
