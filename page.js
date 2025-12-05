e8bc8f69b4f594f86fa7ffc9145e658b81b56288
Kadar1983 <kadar.zh@gmail.com>
2025-12-05 14:06:49 +0100

    Update and rename page.js to app/withdraw/page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Privacy() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">سياسة الخصوصية</h1>
-      <p>هنا نص سياسة الخصوصية...</p>
-    </div>
-  );
-}
