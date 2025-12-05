b7614dc6fbe03ba65c4a36a1cab4673aadf24368
Kadar1983 <kadar.zh@gmail.com>
2025-12-05 14:03:45 +0100

    Update and rename page.js to app/terms/page.js

---
+++ app/terms/page.js
@@ -0,0 +1,8 @@
+export default function Terms() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">الشروط والأحكام</h1>
+      <p>هنا نص الشروط والأحكام...</p>
+    </div>
+  );
+}
