9598854bbc23c67595a2f6d327a37783e1da98a7
Kadar1983 <kadar.zh@gmail.com>
2025-12-05 16:48:12 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,12 @@
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ar">
-      <body className="bg-[#0f1724] text-white min-h-screen">
-        {children}
+      <body>
+        <Navbar />
+        <main className="p-4">{children}</main>
       </body>
     </html>
   );
